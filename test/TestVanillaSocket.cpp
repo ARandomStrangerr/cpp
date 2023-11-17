910bc50028e74accdff6ab80ec9d34fbd9ec5960
thanhdo <billslim0996@gmail.com>
2023-11-17 00:41:16 -0800

    finish test case

--- test/TestVanillaSocket.cpp
+++ test/TestVanillaSocket.cpp
@@ -6,17 +6,23 @@ void listener(){
 	Listener listener (8888, 5); // open a listener at port 8888 with backlog 5
 	Socket socket = listener.acceptConnection(); // accept incoming socket
 	std::string str = "hello\n";
-	socket.write(str.c_str(), str.length());
+	socket.write(str.c_str(), str.length()); // write to stream
+	std::cout<<"Read from server side: "<<socket.readLine()<<std::endl; // read from stream
+	socket.disconnect();
+	listener.disconnect();
 }
 
 void socketTester(){
 	Socket socket("127.0.0.1", 8888);// open a socket at port 8888 with backlog 5
-	std::cout<<socket.readLine()<<std::endl;
+	std::cout<<"Read from client side: "<<socket.readLine()<<std::endl;
+	std::string str = "hello from client\n";
+	socket.write(str.c_str(), str.length());
+	socket.disconnect();
 }
 
 int main(){
 	std::thread t1(listener);
-	usleep(10000);
+	usleep(100000);
 	std::thread t2(socketTester);
 	t1.join();
 	t2.join();
