2cc3cf62f1c500921d7ad3fe238e2276dd8f27b0
thanhdo <billslim0996@gmail.com>
2024-06-13 18:38:42 -0700

    now works with initial velocity

--- particle-sim/main.cpp
+++ particle-sim/main.cpp
@@ -9,8 +9,8 @@ class Obj{
 		sf::CircleShape shape;
 		sf::Vector2f a, prevPos, curPos;
 	public:
-		Obj (float x, float y, float r, sf::Color color): shape(r), prevPos(x,y), curPos(x,y){
-			shape.setPosition(x,y);
+		Obj (float x, float y, float r, sf::Color color, float initX, float initY) : shape(r), prevPos(x,y), curPos(x+initX,y+initY){
+			shape.setPosition(prevPos);
 			shape.setFillColor(color);
 		}
 
@@ -18,7 +18,7 @@ class Obj{
 			//std::cout << shape.getPosition().x << " , " << shape.getPosition().y << std::endl;
 			sf::Vector2f v = curPos - prevPos;
 			prevPos = curPos;
-			curPos = shape.getPosition() + v * dt + a * dt * dt;
+			curPos = curPos + v + a * dt * dt;
 			shape.setPosition(curPos);
 		}
 
@@ -32,9 +32,9 @@ class Obj{
 };
 
 int main() {
-	Obj shape1(400, 100, 10, sf::Color::Cyan);
-	sf::Vector2f gravity(0, 2);
-	float dt = 0.005;
+	Obj shape1(400, 100, 10, sf::Color::Cyan, 0.05, 0);
+	sf::Vector2f gravity(0, 1);
+	float dt = 0.0001;
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
