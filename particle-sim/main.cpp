8ba6121650b0b0f98e628d9f68b32da21c198c4f
thanhdo <billslim0996@gmail.com>
2024-06-15 00:04:20 -0700

    now the shape stay within a boundary

--- particle-sim/main.cpp
+++ particle-sim/main.cpp
@@ -1,9 +1,17 @@
 #include "SFML/Graphics/CircleShape.hpp"
 #include "SFML/Graphics/Color.hpp"
+#include "SFML/Graphics/PrimitiveType.hpp"
+#include "SFML/Graphics/Shape.hpp"
+#include "SFML/Graphics/Vertex.hpp"
+#include "SFML/System/Sleep.hpp"
+#include "SFML/System/Time.hpp"
 #include "SFML/System/Vector2.hpp"
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
 #include <iostream>
-
+#include <thread>
+#include <chrono>
+using namespace std;
 class Obj{
 	private:
 		sf::CircleShape shape;
@@ -22,6 +30,20 @@ class Obj{
 			shape.setPosition(curPos);
 		}
 
+		void correctPos(sf::CircleShape boundary){
+			// obtain the position vector center of the boundary
+			sf::Vector2f centerBoundary = boundary.getPosition() + sf::Vector2f(boundary.getRadius(), boundary.getRadius());
+			// obtain the position vecotr center of this shape
+			sf::Vector2f centerThisShape = shape.getPosition() + sf::Vector2f(shape.getRadius(), shape.getRadius());
+			// if the displacement is greater than the radius, then it is outside of the circle
+			sf::Vector2f displacement = centerThisShape - centerBoundary;
+			float distance = sqrt(pow(displacement.x, 2) + pow(displacement.y, 2));
+			if (distance + shape.getRadius() >= boundary.getRadius()) {
+				sf::Vector2f correctPosition = centerBoundary + (displacement / distance) * (boundary.getRadius() - shape.getRadius()) - sf::Vector2f(shape.getRadius(), shape.getRadius());
+				shape.setPosition(correctPosition);
+			}
+		}
+
 		void accelerate(sf::Vector2f vec) {
 			a += vec;
 		}
@@ -32,9 +54,11 @@ class Obj{
 };
 
 int main() {
-	Obj shape1(400, 100, 10, sf::Color::Cyan, 0.05, 0);
+	Obj shape1(300, 300, 30, sf::Color::Cyan, 0.05, 0);
 	sf::Vector2f gravity(0, 1);
 	float dt = 0.0001;
+	
+	sf::CircleShape container(300);	
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
@@ -46,8 +70,10 @@ int main() {
 
 		shape1.accelerate(gravity);
 		shape1.updatePos(dt);
+		shape1.correctPos(container);
 
 		window.clear();
+		window.draw(container);
 		window.draw(shape1.getShape());
 		window.display();
 	}
