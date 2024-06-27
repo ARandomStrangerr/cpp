7eeec7fda07f384c1226c86d515d0f860903fc5f
thanhdo <billslim0996@gmail.com>
2024-06-26 17:05:24 -0700

    add FPS text onto the screen so we can mornitor the behaviour

--- particle-sim/main.cpp
+++ particle-sim/main.cpp
@@ -3,11 +3,13 @@
 #include "SFML/Graphics/PrimitiveType.hpp"
 #include "SFML/Graphics/Shape.hpp"
 #include "SFML/Graphics/Vertex.hpp"
+#include "SFML/System/Clock.hpp"
 #include "SFML/System/Sleep.hpp"
 #include "SFML/System/Time.hpp"
 #include "SFML/System/Vector2.hpp"
 #include <SFML/Graphics.hpp>
 #include <chrono>
+#include <string>
 #include <thread>
 #include <vector>
 #include <iostream>
@@ -72,20 +74,25 @@ class Obj{
 std::mutex objVecMutex;
 
 void addObjectThread(std::vector<Obj>& objVec){
-	int totalObject = 10;
+	int totalObject = 100;	
 	float r = 5,
 				xIncrement = 0.1,
-				x = 0.5;
+				x = 1,
+				yIncrement = 0.1,
+				y = 0;
 	int colors [15] {205,180,219,255,200,221,255, 175, 204,189, 224, 254,162, 210, 255};
 	for (int i=0; i < totalObject; i++){
 		objVecMutex.lock();	
-		objVec.push_back(Obj (500,300,r, sf::Color(colors[i%5*3], colors[i%5*3+1], colors[i%5*3+2]), 0, 0));
+		objVec.push_back(Obj (200,200, r, sf::Color(colors[i%5*3], colors[i%5*3+1], colors[i%5*3+2]), x, y));
 		objVecMutex.unlock();
 		x+=xIncrement;
 		if (x > 1.5) xIncrement = -0.1;
+		else if (x < 0.5) xIncrement = 0.1;
+		if (y > 0.5) yIncrement = -0.1;
+		else if (y < -0.5) yIncrement = 0.1;
 		r+=5;
-		if (r == 20) r = 10;
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+		if (r == 20) r = 5;
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 }
 
@@ -93,11 +100,22 @@ int main() {
 	std::vector<Obj> objVec;
 
 	sf::Vector2f gravity(0, 9.81);
-	float dt = 0.01;
+	float dt = 0.005;
 	
 	sf::CircleShape container(300);
 
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");	
+	sf::Font font;
+	font.loadFromFile("/System/Library/Fonts/Supplemental/Arial.ttf");
+	sf::Text fpsText;
+	fpsText.setFont(font);
+	fpsText.setCharacterSize(20);
+	fpsText.setFillColor(sf::Color(208, 244, 222));
+	fpsText.setPosition(0,0);
+	sf::Clock clock;
+	float fps = 0;
+
+
+	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML window");
 	
 	std::thread thread(addObjectThread, std::ref(objVec));
 	
@@ -113,7 +131,7 @@ int main() {
 		
 		objVecMutex.lock();
 		for (int i = 0; i < objVec.size(); i++) objVec[i].accelerate(gravity);
-		for (int k = 5; k--;){
+		for (int k = 9; k--;){
 			for (int i = 0; i < objVec.size(); i++) objVec[i].updatePos(dt);
 			for (int i = 0; i < objVec.size(); i++) objVec[i].stayInBoundary(container);
 			for (int i = 0; i < objVec.size(); i++) 
@@ -122,6 +140,10 @@ int main() {
 		}
 		for (int i=0; i<objVec.size();i++) window.draw(objVec[i].getShape());
 		objVecMutex.unlock();
+
+		fps = 1.f / clock.restart().asSeconds();
+		fpsText.setString("FPS: " + std::to_string(static_cast<int>(fps)));
+		window.draw(fpsText);
 
 		window.display();
 
